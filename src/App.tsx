9973ad3b072e0c34496057bb5c9f9147a5b238ee
major6977 <majordav@msn.com>
2023-03-04 11:49:05 -0700

    first commit using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Person} from "./components/Person";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person
+      name="Major"
+      email="majordav@msn.com"
+      age={23}
+      isMarried={true}
+      friends={["Vergil", "Mary", "James"]}
+      />
     </div>
   );
 }
